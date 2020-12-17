09948d3d715ae6f8e832e32cf8bdca96fdb88d53
SvinoteKen <56360706+SvinoteKen@users.noreply.github.com>
2020-12-17 11:31:36 +0300

    Update Player.cpp

--- Lab 4/Player.cpp
+++ Lab 4/Player.cpp
@@ -4,7 +4,6 @@
 Player::Player() {
 	credits_ = 10000;
 	sum_ = 0;
-	new_hand_ = false;
 	splitSum = 0;
 }
 Player::~Player() {
@@ -17,16 +16,11 @@ int Player::getCredits() const {
 void Player::setCredits(int credits) {
 	credits_ = credits;
 }
-void Player::openHand() {
-	hand.reserve(5);
-}
 void Player::addNewHand() {
 	splitHand.reserve(5);
-	new_hand_ = true;
 }
 void Player::deleteNewHand() {
 	splitHand.clear();
-	new_hand_ = false;
 }
 int Player::getSum() {
 	return sum_;
